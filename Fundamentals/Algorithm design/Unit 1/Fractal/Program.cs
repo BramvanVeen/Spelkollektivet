c3b07c948e97388322a1b6605ec860054c58b11f
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-07-08 07:53:09 +0200

    end while

--- Fundamentals/Algorithm design/Unit 1/Fractal/Program.cs
+++ Fundamentals/Algorithm design/Unit 1/Fractal/Program.cs
@@ -7,22 +7,21 @@ namespace Fractal
     {
         static void Main(string[] args)
         {
-            for (int y = -10; y < 10; y++) 
+            for (int y = -10; y < 10; y++)
             {
-            for (int x = 1; x < 80; x++)
+                for (int x = 1; x < 80; x++)
                 {
                     double r = 0;
                     double i = 0;
                     int k = -1;
 
                     while (r * r + i * i < 11 && k < 112)
-                            { 
+                    {
                         double t = r;
                         r = t * t - i * i - 2.3 + x / 24.5;
                         i = 2 * t * i + y / 8.5;
                         k++;
-                            break;
-                            }
+                    }
                     int c = k % 16;
                     Console.BackgroundColor = (ConsoleColor)c;
                     Console.WriteLine("' '");
