19b6bf86d77ef047983fc3de154f5d8924b4e1e5
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-07-10 22:39:10 +0200

    Update Program.cs

--- Fundamentals/Algorithm design/Unit 1/A better Join/Program.cs
+++ Fundamentals/Algorithm design/Unit 1/A better Join/Program.cs
@@ -17,52 +17,55 @@ namespace A_better_Join
                 // We have no items, simply return an empty string.
                 return string.Empty;
             }
-
             else if (count == 1)
             {
                 // We have one item, simply return a single item.
                 return items[0];
             }
-
             else if (count == 2)
             {
                 // We have two items, return two items with "and" in the middle.
                 return string.Join(" and ", items);
             }
-
-            //something with serial comma:
             else
             {
                 var itemsCopy = new List<string>(items);
-
                 if (useSerialComma)
-                //prepend "and" to the last item in the copied list.
-                //return the copied list of items joined with ",".
                 {
-                    itemsCopy[count - 1] = " and " + itemsCopy[count - 1];
+                    //prepend "and " to the last item in the copied list.
+                    itemsCopy[count - 1] = "and " + itemsCopy[count - 1];
                 }
-
-                else //Join the last two items with "and",
-                     //and set this text as the second to last item in the copied list.
-                     //remove the last item in the copied list
-                     //return the copied list of items joined with ",".
+                else
                 {
+                    //Join the last two items with "and",
+                    //and set this text as the second to last item in the copied list.
+                    //remove the last item in the copied list
                     itemsCopy[count - 2] = itemsCopy[count - 2] + " and " + itemsCopy[count - 1];
                     itemsCopy.RemoveAt(itemsCopy.Count - 1);
                 }
-
-                    return
-
-                    string.Join(", ", itemsCopy);
-                }
-            }
-            static void Main(string[] args)
-            {
-                var people = new List<string> { "one", "two", "three", "four", "five", "six" };
-                string aCompleteList = JoinWithAnd(people);
-
-            for (int i = 0; i < people.Count; i++) 
-                Console.WriteLine(aCompleteList);
+                //return the copied list of items joined with ",".
+                return string.Join(", ", itemsCopy);
             }
         }
+        static void Main(string[] args)
+        {
+            var people = new List<string> { "one", "two", "three", "four", "five", "six" };
+
+            while (people.Count >= 0)
+            {
+                Console.WriteLine(JoinWithAnd(people));
+                Console.WriteLine(JoinWithAnd(people, false));
+
+                if (people.Count == 0)
+                {
+                    break;
+                }
+                people.Reverse();
+                people.Remove(people[0]);
+                people.Reverse();
+            }
+
+
+        }
     }
+}
