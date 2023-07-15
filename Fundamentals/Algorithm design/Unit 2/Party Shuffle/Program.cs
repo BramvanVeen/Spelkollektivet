c9587b633925f64e43f637ac2bc3bea8e8c936b7
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-07-15 16:34:28 +0200

    5

--- Fundamentals/Algorithm design/Unit 2/Party Shuffle/Program.cs
+++ Fundamentals/Algorithm design/Unit 2/Party Shuffle/Program.cs
@@ -10,38 +10,36 @@ namespace Party_Shuffle
         //Create a  ShuffleList(List<string> items) method
         //that shuffles the items in a list with the Fisher-Yates modern algorithm version.
         //Call the method and pass it the list of names.
-        static string ShuffleList(List<string> items)
+            static void ShuffleList(List<string> items)
         {
-            {
                 var random = new Random();
                 int numberOfItemsInList = items.Count;
                 while (numberOfItemsInList > 1)
                 {
                     numberOfItemsInList--;
-                    int chosenName = random.Next(numberOfItemsInList + 1);
-
-                    items.Remove(chosenName);
-                    items.AddRange(chosenName);
+                    int chosenNameIndex = random.Next(numberOfItemsInList + 1);
+                Swap(items, chosenNameIndex, 7);
                 }
-            }
-            return items;
+        }
+        public static void Swap<T>(IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
         }
         static void Main(string[] args)
         {
             //Create a list of names in your Main method and fill it with names of participants.
             List<string> names = new List<string> {"Louie", "Bob", "Shlabethany", "Charlie", "Claire", "Spud", "Biffany", "KickBucket"};
             //Output the list of participants.
-            Console.Write($"The participants are: ");
-            foreach (string name in names)
-            {
-                Console.Write( name);
-                Console.Write(", ");
-            }
-            Console.WriteLine();
+            Console.Write($"The participants are: {string.Join(", ", names)} ");
             Console.WriteLine();
             Console.WriteLine($"Generating starting order ...");
+            ShuffleList(names);
             //Output the names again to see the new, random order.
-            Console.WriteLine(ShuffleList(names));
+            Console.WriteLine();
+            Console.Write($"The order is: {string.Join(", ", names)} ");
+            Console.WriteLine();
         }
     }
 }
