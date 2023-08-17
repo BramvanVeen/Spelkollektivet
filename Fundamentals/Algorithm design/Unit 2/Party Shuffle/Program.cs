1aca8fc8ef27cbd1da9f728317c6e045f45542f0
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-08-17 12:06:38 +0200

    17-08-2023

--- Fundamentals/Algorithm design/Unit 2/Party Shuffle/Program.cs
+++ Fundamentals/Algorithm design/Unit 2/Party Shuffle/Program.cs
@@ -7,9 +7,6 @@ namespace Party_Shuffle
 {
     internal class Program
     {
-        //Create a  ShuffleList(List<string> items) method
-        //that shuffles the items in a list with the Fisher-Yates modern algorithm version.
-        //Call the method and pass it the list of names.
             static void ShuffleList(List<string> items)
         {
                 var random = new Random();
@@ -18,25 +15,18 @@ namespace Party_Shuffle
                 {
                     numberOfItemsInList--;
                     int chosenNameIndex = random.Next(numberOfItemsInList + 1);
-                Swap(items, chosenNameIndex, 7);
-                }
-        }
-        public static void Swap<T>(IList<T> list, int indexA, int indexB)
-        {
-            T tmp = list[indexA];
-            list[indexA] = list[indexB];
-            list[indexB] = tmp;
+                string chosenName = items[chosenNameIndex];
+                items.RemoveAt(chosenNameIndex);
+                items.Add(chosenName);
+            }
         }
         static void Main(string[] args)
         {
-            //Create a list of names in your Main method and fill it with names of participants.
-            List<string> names = new List<string> {"Louie", "Bob", "Shlabethany", "Charlie", "Claire", "Spud", "Biffany", "KickBucket"};
-            //Output the list of participants.
+            List<string> names = new List<string> {"Louie", "Bob", "Shlabethany", "Spud", "Charlie", "Claire", "Biffany", "KickBucket"};
             Console.Write($"The participants are: {string.Join(", ", names)} ");
             Console.WriteLine();
             Console.WriteLine($"Generating starting order ...");
             ShuffleList(names);
-            //Output the names again to see the new, random order.
             Console.WriteLine();
             Console.Write($"The order is: {string.Join(", ", names)} ");
             Console.WriteLine();
---
+++ Fundamentals/Algorithm design/Unit 2/Party dilemma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Party_dilemma
+{
+    public class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            if (n <= 1)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+
+
+
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));  
+        }
+    }
+}
