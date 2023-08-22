3610ae04f49d744bcea914df3a3d4e3daf39ccad
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-08-22 09:16:03 +0200

    Update Program.cs

--- Fundamentals/Algorithm design/Unit 3/Adventure map/Program.cs
+++ Fundamentals/Algorithm design/Unit 3/Adventure map/Program.cs
@@ -8,7 +8,8 @@ namespace Adventure_map
         static Random random = new Random();
 
         char[] forest = { 'T', '@', '(', ')', '!', '%', '*' };
-        char forestSymbol = random.Next (forest);
+        char forestSymbol = random.Next(0, forest.Length);
+
 
         char[] border = { '-', '+', '|', };
         char borderHorizontal = border[0];
@@ -24,9 +25,14 @@ namespace Adventure_map
         /*Method to determine if a bridge symbol should be placed in the grid*/
         /*Method for borders*/
 
-        static void generateBorder(bool[,] border, int width, int height, int direction)
+        static void generateBorder(bool[,] border, int width, int height)
         {
-            switch (borderPlacement)
+            if (height == height / height - 1 && width == width / width - 1)
+            {
+                Console.WriteLine(border[1]);
+            }
+
+            /*switch (borderPlacement)
             {
                 case 0: //leftcornerup  
                     {
@@ -38,12 +44,12 @@ namespace Adventure_map
                         borderCornerLeftsideBottom[height, width / width - 1] = true;
                     }
                     break;
-                case 2: //rightcornerup
+                case 2: //rightcornerup ***WRITELINE***
                     {
                         borderCornerRightsideTop[height / height - 1, width] = true;
                     }
                     break;
-                case 3: //rightcornerbottom
+                case 3: //rightcornerbottom ***WRITELINE***
                     {
                         borderCornerRightsideBottom[height, width] = true;
                     }
@@ -66,75 +72,77 @@ namespace Adventure_map
                         borderLeftVertical[startX, up] = true;
                     }
                     break;
-                case 7: //rightvertical
+                case 7: //rightvertical ***WRITELINE***
                     for (int i = height / height; i < height - 1; i++)
                     {
                         borderRightVertical[startX, up] = true;
                     }
-                    break;
-            }
+                    break;*/
         }
-        /*Method for bridge*/
-        /*Method for titel*/
+    }
+    /*Method for bridge*/
+    /*Method for titel*/
 
 
-        /*Method for road*/
-        /*Method for River*/
-        /*Method for extra road*/
+    /*Method for road*/
+    /*Method for River*/
+    /*Method for extra road*/
 
 
-        /*Method for Forest*/
+    /*Method for Forest*/
 
-        static void Map(int width, int height)
+    static void Map(int width, int height)
+    {
+
+        int horizontal = width;
+        int vertical = height;
+
+        char[,] grid = new char[horizontal, vertical];
+
+        int mapQuarter = width / 4;
+        int bridgeStart = width * 3 / 4;
+
+        // //Drawing the map to console with all of the preperation from earlier, going line by line and layer by layer
+        for (int y = 0; y < height; y++)
         {
-            int horizontal = width;
-            int vertical = height;
-
-            char[,] grid = new char[horizontal, vertical];
-
-            int mapQuarter = width / 4;
-            int bridgeStart = width * 3 / 4;
-
-            // //Drawing the map to console with all of the preperation from earlier, going line by line and layer by layer
-            for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
             {
-                for (int x = 0; x < width; x++)
+                generateBorder();
+                /*switch (location on map)
                 {
-                    /*switch (location on map)
-                    {
-                        case 0: River
-                                code block (Maybe it's own method?) 
-                                (Then if river is true for that position, place the correct symbol)
-                                break;
-                        case 1: Forest
-                                code block (If Forest is true for that position, place the correct symbol)
-                                break;    
-                        case 3: Road 
-                                code block (If Road is true for that position, place the correct symbol)
-                                break;
-                        case 4: Bridge
-                                code block (If Bridge is true for that position, place the correct symbol)
-                                break;
-                        case 5: Border
-                                code block (If Border is true for that position, place the correct symbol)
-                                break;
-                        default: Empty
-                            code block(If Empty is true for that position, consolewriteline" ";)
-                            break;*/
+                    case 0: River
+                            code block (Maybe it's own method?) 
+                            (Then if river is true for that position, place the correct symbol)
+                            break;
+                    case 1: Forest
+                            code block (If Forest is true for that position, place the correct symbol)
+                            break;    
+                    case 3: Road 
+                            code block (If Road is true for that position, place the correct symbol)
+                            break;
+                    case 4: Bridge
+                            code block (If Bridge is true for that position, place the correct symbol)
+                            break;
+                    case 5: Border
+                            code block (If Border is true for that position, place the correct symbol)
+                            break;
+                    default: Empty
+                        code block(If Empty is true for that position, consolewriteline" ";)
+                        break;*/
+            }
+
+
+
+        }
+        Console.WriteLine();
     }
 
-
-
 }
-                Console.WriteLine();
-            }
-
-        }
-        static void Main(string[] args)
-        {
-            //Calling the map method with sizes, first for width, second for height. 
-            Map(20, 10);
-        }
+static void Main(string[] args)
+{
+    //Calling the map method with sizes, first for width, second for height. 
+    Map(20, 10);
+}
 
     }
 }
