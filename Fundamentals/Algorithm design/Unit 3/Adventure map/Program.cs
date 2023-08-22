594171185f9588efbe398fb24bf177d2c1cc421b
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-08-22 10:57:42 +0200

    Update Program.cs

--- Fundamentals/Algorithm design/Unit 3/Adventure map/Program.cs
+++ Fundamentals/Algorithm design/Unit 3/Adventure map/Program.cs
@@ -6,143 +6,46 @@ namespace Adventure_map
     internal class Program
     {
         static Random random = new Random();
-
-        char[] forest = { 'T', '@', '(', ')', '!', '%', '*' };
-        char forestSymbol = random.Next(0, forest.Length);
-
-
-        char[] border = { '-', '+', '|', };
-        char borderHorizontal = border[0];
-        char borderVertical = border[2];
-        char borderCorner = border[1];
-
-        char[] River = { '/', '|', '╲' };
-        char[] Road = { '*' };
-        char[] Bridge = { '=' };
-        char[] Empty = { ' ' };
-
-        //In this order (order doesnt matter maybe for methods)
-        /*Method to determine if a bridge symbol should be placed in the grid*/
-        /*Method for borders*/
-
-        static void generateBorder(bool[,] border, int width, int height)
+        
+        static void Map(int width, int height)
         {
-            if (height == height / height - 1 && width == width / width - 1)
+            char[,] grid = new char[width, height];
+            int mapQuarter = width / 4;
+            int bridgeStartx = width * 3 / 4;
+
+            void GenerateBorders(char[,] grid)
             {
-                Console.WriteLine(border[1]);
-            }
-
-            /*switch (borderPlacement)
-            {
-                case 0: //leftcornerup  
-                    {
-                        borderCornerLeftsideTop[height/height-1, width/width-1] = true;
-                    }
-                    break;
-                case 1: //leftcornerbottom
-                    {
-                        borderCornerLeftsideBottom[height, width / width - 1] = true;
-                    }
-                    break;
-                case 2: //rightcornerup ***WRITELINE***
-                    {
-                        borderCornerRightsideTop[height / height - 1, width] = true;
-                    }
-                    break;
-                case 3: //rightcornerbottom ***WRITELINE***
-                    {
-                        borderCornerRightsideBottom[height, width] = true;
-                    }
-                    break;
-                case 4: //tophorizontal
-                    for (int i = width/width; i < width-1; i++)
-                    {
-                        borderhorizontalTop[,] = true;
-                    }
-                    break;
-                case 5: //bottomhorizontal
-                    for (int i = width/width; i < width-1; i++)
-                    {
-                        borderhorizontalBottom[startX, up] = true;
-                    }
-                    break;
-                case 6: //leftvertical
-                    for (int i = height / height; i < height - 1; i++) 
-                    {
-                        borderLeftVertical[startX, up] = true;
-                    }
-                    break;
-                case 7: //rightvertical ***WRITELINE***
-                    for (int i = height / height; i < height - 1; i++)
-                    {
-                        borderRightVertical[startX, up] = true;
-                    }
-                    break;*/
-        }
-    }
-    /*Method for bridge*/
-    /*Method for titel*/
-
-
-    /*Method for road*/
-    /*Method for River*/
-    /*Method for extra road*/
-
-
-    /*Method for Forest*/
-
-    static void Map(int width, int height)
-    {
-
-        int horizontal = width;
-        int vertical = height;
-
-        char[,] grid = new char[horizontal, vertical];
-
-        int mapQuarter = width / 4;
-        int bridgeStart = width * 3 / 4;
-
-        // //Drawing the map to console with all of the preperation from earlier, going line by line and layer by layer
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                generateBorder();
-                /*switch (location on map)
+                grid[0, 0] = '+';
+                grid[0, height - 1] = '+';
+                grid[width - 1, 0] = '+';
+                grid[width - 1, height - 1] = '+';
+                for (int x = 1; x < width - 1; x++)
                 {
-                    case 0: River
-                            code block (Maybe it's own method?) 
-                            (Then if river is true for that position, place the correct symbol)
-                            break;
-                    case 1: Forest
-                            code block (If Forest is true for that position, place the correct symbol)
-                            break;    
-                    case 3: Road 
-                            code block (If Road is true for that position, place the correct symbol)
-                            break;
-                    case 4: Bridge
-                            code block (If Bridge is true for that position, place the correct symbol)
-                            break;
-                    case 5: Border
-                            code block (If Border is true for that position, place the correct symbol)
-                            break;
-                    default: Empty
-                        code block(If Empty is true for that position, consolewriteline" ";)
-                        break;*/
+                    grid[x, 0] = '-';
+                }
+                for (int y = 1; y < height - 1; y++)
+                {
+                    grid[y, 0] = '|';
+                }
+                //GenerateBorders(grid);
             }
+            GenerateBorders(grid);
 
-
-
+            // //Drawing the map to console with all of the preperation from earlier, going line by line and layer by layer
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(".");
+                }
+                Console.WriteLine(".");
+            }
         }
-        Console.WriteLine();
+        
+        static void Main(string[] args)
+    {
+        //Calling the map method with sizes, first for width, second for height. 
+        Map(12, 12);
     }
-
 }
-static void Main(string[] args)
-{
-    //Calling the map method with sizes, first for width, second for height. 
-    Map(20, 10);
-}
-
-    }
 }
