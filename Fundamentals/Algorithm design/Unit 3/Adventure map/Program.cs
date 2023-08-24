b06789613ab54cc9d842400258896dc8b4a0edd8
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-08-24 15:15:31 +0200

    Update Program.cs

--- Fundamentals/Algorithm design/Unit 3/Adventure map/Program.cs
+++ Fundamentals/Algorithm design/Unit 3/Adventure map/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 
 namespace Adventure_map
@@ -13,36 +14,39 @@ namespace Adventure_map
             //Declare grid data structure
             char[,] grid = new char[width, height];
 
-            //Prepare the grid
+            //Prepare the forest
+            char[] forestSymbols = { 'T', '@', '(', ')', '!', '%', '*', ' ', ' ', ' ' };
             int mapQuarter = width / 4;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 1; x < mapQuarter - 1; x++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    int forestSymbolIndex = random.Next(forestSymbols.Length);
+                    grid[x, y] = forestSymbols[forestSymbolIndex];
+                }
+            }
 
             //Prepare the border
             grid[0, 0] = '+';
             grid[0, height - 1] = '+';
             grid[width - 1, 0] = '+';
-            grid[width-1, height-1] = '+';
+            grid[width - 1, height - 1] = '+';
 
             for (int x = 1; x < width - 1; x++)
             {
                 grid[x, 0] = '-';
+                grid[x, height - 1] = '-';
             }
             for (int y = 1; y < height - 1; y++)
             {
                 grid[0, y] = '|';
-            }
-
-            for (int y = 1; y < height - 1; y++)
-            {
-                grid[width-1, y] = '|';
-            }
-
-            for (int x = 1; x < width - 1; x++)
-            {
-                grid[x, height - 1] = '-';
+                grid[width - 1, y] = '|';
             }
 
             //Prepare the Title:
-            grid[width / 2 - 6, 1] = 'A';
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            grid[width / 2 - 6, 1] =  'A';
             grid[width / 2 - 5, 1] = 'D';
             grid[width / 2 - 4, 1] = 'V';
             grid[width / 2 - 3, 1] = 'E';
@@ -56,88 +60,92 @@ namespace Adventure_map
             grid[width / 2 + 5, 1] = 'A';
             grid[width / 2 + 6, 1] = 'P';
 
-            //Prepare the Bridge:
+            //Prepare the Bridge and subsequent markerpoints:
             int bridgeStartx = width * 3 / 4;
-            int markerPointx = bridgeStartx-1;
+            int markerPointx = bridgeStartx - 1;
             int bridgeStarty = random.Next(7, height - 7);
             int markerPointy = bridgeStarty + 1;
             int secondMarkerPointxForRoadGoingRight = markerPointx + 5;
             int secondMarkerPointyForRoadGoingRight = markerPointy;
-
             int riverMarkerx = bridgeStartx + 2;
             int rivertMarkery = bridgeStarty + 3;
             int secondRiverMarkerx = bridgeStartx + 2;
-            int secondRiverMarkery = bridgeStarty - 3;
-
-
+            int secondRiverMarkery = bridgeStarty - 1;
+            int sideRoadMarkerx = markerPointx;
+            int sideRoadMarkery = markerPointy + 1;
+            Console.ForegroundColor = ConsoleColor.Red;
             grid[bridgeStartx, bridgeStarty + 1] = '#';
             grid[bridgeStartx + 1, bridgeStarty + 1] = '#';
             grid[bridgeStartx + 2, bridgeStarty + 1] = '#';
             grid[bridgeStartx + 3, bridgeStarty + 1] = '#';
             grid[bridgeStartx + 4, bridgeStarty + 1] = '#';
+            grid[sideRoadMarkerx, sideRoadMarkery] = '#';
 
             for (int y = 0; y < 5; y++)
-            { 
+            {
                 grid[bridgeStartx, bridgeStarty] = '=';
                 bridgeStartx++;
             }
             for (int y = 0; y < 5; y++)
-            { 
-                grid[bridgeStartx-5, bridgeStarty + 2] = '='; 
+            {
+                grid[bridgeStartx - 5, bridgeStarty + 2] = '=';
                 bridgeStartx++;
             }
 
             //Road going Left
-            while (markerPointx>0)
+            while (markerPointx > 0)
             {
                 grid[markerPointx, markerPointy] = '#';
                 markerPointy += random.Next(0, 3) - 1;
                 markerPointx--;
             }
             //Road going right
-            while (secondMarkerPointxForRoadGoingRight<width-1)
+            while (secondMarkerPointxForRoadGoingRight < width - 1)
             {
                 grid[secondMarkerPointxForRoadGoingRight, secondMarkerPointyForRoadGoingRight] = '#';
-                secondMarkerPointyForRoadGoingRight += random.Next(0, 3) -1;
+                secondMarkerPointyForRoadGoingRight += random.Next(0, 3) - 1;
                 secondMarkerPointxForRoadGoingRight++;
             }
             //River flowing down
-            while (rivertMarkery < height-1)
+            while (rivertMarkery < height - 1)
             {
                 int directionModifier = random.Next(0, 3) - 1;
                 if (directionModifier < 0)
                 {
                     grid[riverMarkerx, rivertMarkery] = '/';
-                    grid[riverMarkerx-1, rivertMarkery] = '/';
-                    grid[riverMarkerx+1, rivertMarkery] = '/';
+                    grid[riverMarkerx - 1, rivertMarkery] = '/';
+                    grid[riverMarkerx + 1, rivertMarkery] = '/';
+                    grid[riverMarkerx - 3, rivertMarkery] = '#';
                 }
                 else if (directionModifier > 0)
                 {
                     grid[riverMarkerx, rivertMarkery] = '\\';
-                    grid[riverMarkerx-1, rivertMarkery] = '\\';
-                    grid[riverMarkerx+1, rivertMarkery] = '\\';
+                    grid[riverMarkerx - 1, rivertMarkery] = '\\';
+                    grid[riverMarkerx + 1, rivertMarkery] = '\\';
+                    grid[riverMarkerx - 3, rivertMarkery] = '#';
                 }
                 else
-                { 
+                {
                     grid[riverMarkerx, rivertMarkery] = '|';
-                    grid[riverMarkerx-1, rivertMarkery] = '|';
-                    grid[riverMarkerx+1, rivertMarkery] = '|';
+                    grid[riverMarkerx - 1, rivertMarkery] = '|';
+                    grid[riverMarkerx + 1, rivertMarkery] = '|';
+                    grid[riverMarkerx - 3, rivertMarkery] = '#';
                 }
-                
+
                 riverMarkerx = riverMarkerx + directionModifier;
                 rivertMarkery++;
             }
             //River flowing up
             while (secondRiverMarkery > 1)
             {
-                int directionModifier = random.Next(0, 3) - 1;
-                if (directionModifier < 0)
+                int directionModifier2 = random.Next(0, 3) - 1;
+                if (directionModifier2 > 0)
                 {
                     grid[secondRiverMarkerx, secondRiverMarkery] = '/';
                     grid[secondRiverMarkerx - 1, secondRiverMarkery] = '/';
                     grid[secondRiverMarkerx + 1, secondRiverMarkery] = '/';
                 }
-                else if (directionModifier > 0)
+                else if (directionModifier2 < 0)
                 {
                     grid[secondRiverMarkerx, secondRiverMarkery] = '\\';
                     grid[secondRiverMarkerx - 1, secondRiverMarkery] = '\\';
@@ -149,16 +157,18 @@ namespace Adventure_map
                     grid[secondRiverMarkerx - 1, secondRiverMarkery] = '|';
                     grid[secondRiverMarkerx + 1, secondRiverMarkery] = '|';
                 }
-                secondRiverMarkerx = secondRiverMarkerx + directionModifier;
-                rivertMarkery--;
+                secondRiverMarkerx = secondRiverMarkerx + directionModifier2;
+                secondRiverMarkery--;
             }
+
             //Drawing the map to console with all of the preperation from earlier, going line by line and layer by layer
             for (int y = 0; y < height; y++)
 
             {
                 for (int x = 0; x < width; x++)
                 {
-                    
+
+
                     if (grid[x, y] == '\0')
                     {
                         Console.Write(" ");
