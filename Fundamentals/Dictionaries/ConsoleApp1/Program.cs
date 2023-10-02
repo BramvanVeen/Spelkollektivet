f08e1ecbf1705b9b6fba6ba90b67d0fe35bbdf1d
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-10-02 09:31:19 +0200

    02102023

--- Fundamentals/Dictionaries/ConsoleApp1/Program.cs
+++ Fundamentals/Dictionaries/ConsoleApp1/Program.cs
@@ -1,64 +1,52 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Runtime.Intrinsics.X86;
 
-namespace ConsoleApp1
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Create a Dictionary that maps years to host countries:
+        Dictionary<int, string> winterOlympicHostCountries = new Dictionary<int, string>();
+
+        winterOlympicHostCountries[2002] = "United States";
+        winterOlympicHostCountries[2006] = "Italy";
+        winterOlympicHostCountries[2010] = "Canada";
+        winterOlympicHostCountries[2014] = "Russia";
+        winterOlympicHostCountries[2018] = "South Korea";
+        winterOlympicHostCountries[2022] = "China";
+
+        //To generate a random year, a list of years using the Keys property of the dictionary, as suggested in the hint:
+        var year = new List<int>(winterOlympicHostCountries.Keys);
+        //Use a random number generator to select a random country from the list:
+        Random random = new Random();
+
+        while (true)
         {
-            Dictionary<int, string> winterOlympicHostCountries = new Dictionary<int, string>();
-
-            
-            winterOlympicHostCountries[2002] = "United States";
-            winterOlympicHostCountries[2006] = "Italy";
-            winterOlympicHostCountries[2010] = "Canada";
-            winterOlympicHostCountries[2014] = "Russia";
-            winterOlympicHostCountries[2018] = "South Korea";
-            winterOlympicHostCountries[2022] = "China";
-
-            Random random = new Random();
-            int[] values = { 0, 1, 2, 3, 4, 5 };
-            int randomIndex = random.Next(values.Length);
-            int selectedYear = 0;
-            switch (values[randomIndex])
-            {
-                case 0:
-                    selectedYear = 2002;
-                    break;
-                case 1:
-                    selectedYear = 2006;
-                    break;
-                case 2:
-                    selectedYear = 2010;
-                    break;
-                case 3:
-                    selectedYear = 2014;
-                    break;
-                case 4:
-                    selectedYear = 2018;
-                    break;
-                case 5:
-                    selectedYear = 2022;
-                    break;
-            }
-            Console.WriteLine($"Which country hosted the Winter Olympic Games in {selectedYear}? Type your answer, uppercase sensitive..");
+            int randomIndex = random.Next(year.Count);
+            int randomYear = year[randomIndex];
+            //Ask the player to provide the country that hosted the winter olympics:
+            Console.Write($"Which country hosted the Winter Olympic Games in {randomYear}? Type your answer, uppercase sensitive..\" ");
             string playerAnswer = Console.ReadLine();
+            //Check the player's answer against the dictionary and respond accordingly:
 
-            if (winterOlympicHostCountries.TryGetValue(selectedYear, out string correctAnswer))
+            if ((winterOlympicHostCountries.TryGetValue(randomYear, out string correctAnswer)) && (playerAnswer.Equals(correctAnswer, StringComparison.OrdinalIgnoreCase)))
             {
-                if (playerAnswer.Equals(correctAnswer, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine("Correct!");
-                }
-                else
-                {
-                    Console.WriteLine($"Incorrect. It was {correctAnswer}.");
-                }
+                Console.WriteLine("Correct!");
             }
             else
             {
-                Console.WriteLine("Invalid year or not in the 21st century.");
+                Console.WriteLine($"Incorrect. It is {correctAnswer}.");
+            }
+
+            Console.Write("Play again? (yes/no): ");
+            string playAgain = Console.ReadLine().Trim().ToLower();
+
+            if (playAgain != "yes")
+            {
+                break; // Exit the loop if the player doesn't want to play again
             }
         }
     }
